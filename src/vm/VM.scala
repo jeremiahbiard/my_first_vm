0895428f0756edcda118301a344098986aa263a2
Jeremiah Biard <jeremiahbiard@gmail.com>
2015-01-20 16:50:54 -0500

    Rewriting the vm in a purely functional style

--- src/vm/VM.scala
+++ src/vm/VM.scala
@@ -7,6 +7,61 @@ import System.err.println
 object VM {
 }
   
+
+class VM() {
+  def push(data: Int, sp: Int, stack: Array[Int]) = {
+    stack(sp) = data
+  }
+  
+  def pop(sp: Int, stack: Array[Int]): Int = {
+    stack(sp)
+  }
+  
+   def disassemble(program: List[Int], opcode: Instruction, ip: Int) = {
+        printf("%04d: %s", ip, opcode)
+        opcode.n match {
+          case 1 =>
+            printf(" %d", program(ip + 1))
+          case 2 =>
+            printf(" %d, %d", program(ip + 1), program(ip + 2))
+          case _ => ""
+        } 
+        printf("\n")
+    }
+  
+  def cpu(program: List[Int], ip: Int, sp: Int, stack: Array[Int]): Unit = {
+    
+    if (ip > program.length) return else {
+    
+      // val trace: Boolean = true
+      val trace: Boolean = false
+      val opcode: Int = program(ip)
+      val instr: Instruction = Bytecode.opcodes(opcode)
+    
+      if (trace) disassemble(program, instr, ip)
+    
+      opcode match {
+        case ICONST =>
+          val new_sp = sp + 1
+          val data_addr = ip + 1
+          val new_ip = ip + 2
+          stack(new_sp) = program(data_addr)
+          cpu(program, new_ip, new_sp, stack)
+        case PRINT =>
+          println(stack(sp))
+          val new_sp = sp - 1
+          val new_ip = ip + 1
+          cpu(program, new_ip, new_sp, stack)
+        case HALT =>
+          println("Program execution halted.")
+        case _ => println("BARF")
+      }
+        
+    }
+  }
+}
+
+/*
  class VM(code: List[Int], main: Int, dataSize: Int) {
   val trace: Boolean = true
   private var stack: Array[Int] = new Array(100: Int)
@@ -75,4 +130,5 @@ object VM {
     }
     
   }
- }
+}
+*/
--- src/vm/testVM.scala
+++ src/vm/testVM.scala
@@ -5,28 +5,17 @@ import vm.Bytecode._
 
 object testVM {
   
+  val test_iconst: List[Int] = List(ICONST, 99, PRINT, HALT)
   val test_add: List[Int] = List(IADD, 2, 2, PRINT, HALT)
   val test_sub: List[Int] = List(ISUB, 4, 2, PRINT, HALT)
   val test_mul: List[Int] = List(IMUL, 4, 4, PRINT, HALT)
   def main(args: Array[String]) = {
+    val vm = new VM()
+    val main = 0
+    var stack = Array(100: Int)
+    vm.cpu(test_iconst, main, -1, stack)
     
-    println("Testing Addition:")
-    println("-----------------")
-    val vm_add: VM = new VM(Nil, 0, 0)
-    // vm.data(1) = HALT
-    vm_add.cpu(test_add)
-    
-    println("")
-    println("Testing Subtraction:")
-    println("--------------------")
-    val vm_sub: VM = new VM(Nil, 0, 0)
-    vm_sub.cpu(test_sub)
-    
-    println("")
-    println("Testing Multiplication:")
-    println("-----------------------")
-    val vm_mul: VM = new VM(Nil, 0, 0)
-    vm_mul.cpu(test_mul)
+
     
   }
 
